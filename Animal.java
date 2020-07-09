cc32e9ea3a6d9e1cd3a58aabaa45df6b3ba9b4a2
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-07-09 15:10:53 +0800

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,10 @@
+public class Animal{
+
+    private String name;
+    int age;
+
+    void introduceYourself() {
+      System.out.println("Morr. I am an animal.");
+      System.out.println("and I am " + this.age + " years old.");
+    }
+}
