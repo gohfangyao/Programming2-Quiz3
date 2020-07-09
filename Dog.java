43f5e80e50c81c88b590335a6734b934b5898115
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-07-09 15:11:47 +0800

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,26 @@
+public class Dog extends Animal {
+   
+    private String name;
+    
+    public Dog(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public Dog (int age){
+        this.age = age;
+    }
+    
+    public int getAge(){
+        return this.age;
+    }
+    
+    @Override
+    public void introduceYourself(){
+        System.out.println("Woof. I am a Dog. My name is " + this.getName());
+        System.out.println("and I am " + this.age + " years old.");
+    }
+}
